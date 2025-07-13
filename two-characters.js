47eccf23da230d60db0ad14f68d797d6284918c0
maimoona akhter <maimoonaakhter@maimoonas-MacBook-Air.local>
2025-07-14 00:06:54 +0500

    two-charaters

---
+++ two-characters.js
@@ -0,0 +1,26 @@
+
+function alternate(s) {
+    // Write your code here
+    let a = [...new Set(s)]
+    console.log('a',a)
+    let b = '';
+    let max = 0;
+   for (let i = 0 ;  i < a.length;i++){
+     for (let j = i+1;j < a.length;j++){
+        b = [...s].filter(item => item === a[i] || item === a[j])
+        console.log(b)
+        let isValid =true
+     for (let k = 0; k < b.length;k++){
+       if (b[k] === b[k+1]){
+       isValid = false
+       console.log(isValid)
+       break
+       }
+     }
+     if (isValid === true &&  b.length > max)
+     max = b.length
+     }
+     }
+return max
+}
+console.log(alternate('abaacdabd'))
